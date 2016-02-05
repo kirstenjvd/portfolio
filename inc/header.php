da9a8e21281d0d1f16d50cdf2ede2109720363d8
Kirsten Dodd <kirsten.jvd@gmail.com>
2016-02-05 02:21:42 -0800

    portfolio update

--- inc/header.php
+++ inc/header.php
@@ -1,21 +1,10 @@
 <div class="header">
-
-	<ul class=" nav nav-pills">
-	  <li><a href="#">Brands</a></li>
-	  <li><a href="#">Sport Shirts</a></li>
-	  <li><a href="#">Dress Shirts</a></li>
-	  <li><a href="#">Workwear</a></li>
-	  <li><a href="#">Tshirts</a></li>
-	  <li><a href="#">Ladies</a></li>
-	  <li><a href="#">Activewear</a></li>
-	  <li><a href="#">Youth</a></li>
-	  <li><a href="#">Fleece</a></li>
-	  <li><a href="#">Outerwear</a></li>
-	  <li><a href="#">Headerwear</a></li>
-	  <li><a href="#">Bags</a></li>
-	  <li><a href="#">Bottoms</a></li>
-	  <li><a href="#">Promo Prod</a></li>
-		
-	</ul>
-
+	<div class="fc wrapper">
+		<img src="images/logo_initials.jpg">
+<!-- 		<ul class=" nav nav-pills">
+		  <li><a href="safety.php">About</a></li>
+		  <li><a href="safety.php">Work</a></li>
+		  <li><a href="contact.php">Contact</a></li>
+		</ul> -->
+	</div>
 </div>
--- index.php
+++ index.php
@@ -5,32 +5,22 @@
       <div class="bg-container"><div class="inner-container"></div></div>
       <div class="jumbotron">
         <!-- <h1>'Allo, 'Allo!</h1> -->
-        <h1 class="lead">Kirsten Dodd</h1>
+        <h1 class="lead">Kirsten <span>Dodd</span></h1>
+        <p class="tagline">Web <em>Designer</em> <strong>Developer</strong> <span>Extraordinaire</span></p>
       </div>
-
       <div class="section">
         <div class="fc wrapper col-lg-6">
-          <div class="fl inner-container"><h2>What I do</h2>
-          <p>I am a design inclined front end developer. I pride myself on doing clean, innovative, and creative work. I am always looking to implement the newest cutting edge web trends.
-</p>
-<p>
-Specialties: HTML5, CSS3, Responsive development</p>
-          </div>
-          
+          <h2>What I do</h2>
+          <p>I am a design inclined front end developer. I pride myself on doing clean, innovative, and creative work. I am always looking to implement the newest cutting edge web trends.</p>
+          <p>Specialties: HTML5, CSS3, Responsive development</p>          
         </div>
       </div>
       <div class="section career-tout">
         <div class="wrapper">
           <h2>Interested in making your website mobile friendly? </h2>
-          <a href="#" class="btn-primary">Contact me! </a>
+          <a href="mailto:kirsten.jvd@gmail.com" class="btn-primary">Contact me! </a>
           <footer class="fc">
-            <ul class="fl footer-links">
-              <li><a href="#">Safety</a></li>
-              <li><a href="#">Services</a></li>
-              <li><a href="#">Careers</a></li>
-              <li><a href="#">Contact</a></li>
-            </ul>
-            <p class="fr copyright">&copy;copyright 2015</p>
+            <p class="fr copyright">&copy;copyright 2016</p>
           </footer>
         </div>
 
