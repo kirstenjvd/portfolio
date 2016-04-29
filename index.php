5a3b7054e227f3cfc632a7a5fe7838dce4f2588a
Kirsten Dodd <kirsten.jvd@gmail.com>
2016-04-28 23:22:02 -0700

    portfolio updated bio

--- index.php
+++ index.php
@@ -12,28 +12,62 @@
       <?php include('inc/header.php'); ?>
       <div class="section" id="section-about">
         <div class="fc wrapper">
-          <div class="container-box type-1">
+          <div class="container-box type-1" id="about">
             <div class="container-img">
               <img src="images/me.jpg" src="picture of me" />
             </div>
             <h2>who</h2>
             <div class="container-copy">
-              <p>I am a design inclined front end developer. I pride myself on doing clean, innovative, and creative work. I am always looking to implement the newest cutting edge web trends.</p>
+              <p>A Freelance <strong>Web Designer and Developer</strong>. An entrepreneurial spirit with a passion for <strong>photography</strong>. A detail oriented trendsetter.  A <strong>mentor</strong>. And above all a <strong>dog lover.</strong></p>
             </div>
           </div>
           
-          <div class="container-box type-2">
+          <div class="container-box type-2" id="work">
             <div class="container-img">
-              <img src="images/desk.jpg" src="picture of me" />
+              <a href="http://dev.portal.esidev.ca/" target="_blank">
+                <img src="images/work_example.jpg" src="Demo site image" />
+              </a>
+            </div>
+            <div class="container-img-2">
+              <a href="http://dev.wpbo.info/" target="_blank">
+                <img src="images/work_example2.jpg" src="paintball site image" />
+              </a>
             </div>
             <h2>what</h2>
             <div class="container-copy">
-              <p>I am currently in the business of creating unique websites with clean designs, responsive development, and integrating into a modern ASP.NET web content management platform using custom templates and widgets.</p>  </p>
+              <p>Kirsten worked as a Senior Web Developer for Toronto Web Agency <a href="http://www.climaxmedia.com" target="_blank">Climax Media</a>. She was lead <strong>Front-End Developer</strong> on the recent redesign of <a href="http://www.honda.ca" target="_blank">Honda Canada's</a> home and models pages.  She was also lead Front-End Developer on projects such as <a href="http://brookfieldhomes.ca/community/treetops/new-homes" target="_blank">Brookfield Homes Community</a>, <a href="http://www.scoutt.com" target="_blank">Scoutt</a>, <a href="http://www.nerdblock.com" target="_blank">Nerd Block</a>, and others. Recently as a <strong>freelance artist</strong> she has been contracted by a Microsoft partner to Design and Develop a <a href="http://dev.portal.esidev.ca/" target="_blank">demo site</a> for potential clients, a student portal for non-profit organization Women Building Futures <em>(in progress)</em>, and a registration system for worldwide pro <a href="http://dev.wpbo.info" target="_blank">Paintball</a> players. In addition, she is currently working with up-and-coming Musicians in Victoria, BC to improve thier marketing and branding through Photography, Digital Design, Social Media Presence, and Website Development.</p>  
             </div>
           </div>
+          <div class="container-box type-3">
 
-          <!-- <p style="font-size: 14px;"><em>Work examples available upon request</em></p>   -->
-
+            <h2>when</h2>
+            <div class="container-copy">
+              <p> 2006 - 2011 <strong>Multi Media BA</strong> - <em>University of New Brunswick</em><br />
+                  2011 - 2012 <strong>Web Design, Development, and Maintenance</strong> - <em>Humber College</em><br />
+                  2012 - 2016 <strong>Climax Media</strong> - <em>Toronto based Web Agency</em><br />
+                  2011 - 2016 Part-time <strong>Freelance</strong> extraordinaire <br />
+                  Present day Full-time <strong>Freelance</strong>  extraordinaire</p>
+            </div>
+          </div>
+          
+          <div class="container-box type-2 where">
+            <div class="container-img">
+              <img src="images/desk.jpg" src="picture of me" />
+            </div>
+            <h2>where</h2>
+            <div class="container-copy">
+              <p>A true gypsy soul; Kirsten has lived in many cities across North America, always finding new sources of creativity and innovation in each.   </p>
+            </div>
+          </div>
+          <div class="container-box type-1 how">
+            <h2>how</h2>
+            <div class="container-copy">
+              <p>
+                <strong>Photography, Photoshop, HTML, CSS, Jquery, JavaScript</strong><br />
+                As a design inclined developer Kirsten understands that form and function are not mutually exclusive and refuses to sacrifice one for the other. Combining her background in development and lifelong passion in digital media, photography, and design; she breathes life into every project she takes on. 
+              </p>
+            </div>
+          </div>
         </div>
       </div>
       <div class="section career-tout">
