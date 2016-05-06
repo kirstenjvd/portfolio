80db444079ee578e4f5834d3cf2007caab952dca
Kirsten Dodd <kirsten.jvd@gmail.com>
2016-05-06 00:53:13 -0700

    adding contact form

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include('inc/contact_form.php'); ?>
 <?php include('inc/application_top.php'); ?>
 
     <div class="homepage">
@@ -70,17 +71,60 @@
           </div>
         </div>
       </div>
+      <div class="section contact-form">
+        <div class="wrapper">
+        <h2>Drop Me a Line</h2>
+          <form class="form-horizontal" role="form" method="post" action="index.php">
+            <div class="form-group name-group">
+              <label for="name" class="col-sm-2 control-label">Name</label>
+              <div class="col-sm-10">
+                <input type="text" class="form-control" id="name" name="name" placeholder="First & Last Name" value="<?php echo htmlspecialchars($_POST['name']); ?>">
+                <?php echo "<p class='text-danger'>$errName</p>";?>
+              </div>
+            </div>
+            <div class="form-group email-group">
+              <label for="email" class="col-sm-2 control-label">Email</label>
+              <div class="col-sm-10">
+                <input type="email" class="form-control" id="email" name="email" placeholder="example@domain.com" value="<?php echo htmlspecialchars($_POST['email']); ?>">
+                <?php echo "<p class='text-danger'>$errEmail</p>";?>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="message" class="col-sm-2 control-label">Message</label>
+              <div class="col-sm-10">
+                <textarea class="form-control" rows="4" name="message" placeholder=""><?php echo htmlspecialchars($_POST['message']);?></textarea>
+                <?php echo "<p class='text-danger'>$errMessage</p>";?>
+              </div>
+            </div>
+            <div class="form-group test-group">
+              <label for="human" class="col-sm-2 control-label">1 + 2 = ?</label>
+              <div class="col-sm-10">
+                <input type="text" class="form-control" id="human" name="human" placeholder="Your Answer">
+                <?php echo "<p class='text-danger'>$errHuman</p>";?>
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="col-sm-10 col-sm-offset-2">
+                <input id="submit" name="submit" type="submit" value="Send" class="btn btn-primary">
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="col-sm-10 col-sm-offset-2">
+                <?php echo $result; ?>  
+              </div>
+            </div>
+          </form> 
+        </div>
+      </div>
       <div class="section career-tout">
         <div class="wrapper">
           <h2>Your business lacking a website or your current website need a facelift? </h2>
-          <p>Contact <a href="mailto:kirsten.jvd@gmail.com" class="btn-primary">kirstenjvd@gmail.com</a> for a quote!</p>
+           <a href="mailto:kirsten.jvd@gmail.com" class="btn-primary">Contact kirstenjvd@gmail.com</a>
           <footer class="fc">
             <p class="copyright">&copy;copyright 2016</p>
           </footer>
         </div>
-
       </div>
     </div>  
-
 
 <?php include('inc/application_bottom.php'); ?>
