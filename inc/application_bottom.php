fde05c2a103fb9532cee50c3f689aac45c72a295
Kirsten Dodd <kirsten.jvd@gmail.com>
2017-03-22 23:11:54 -0700

    page speed updates

--- inc/application_bottom.php
+++ inc/application_bottom.php
@@ -1,3 +1,6 @@
+    <link rel="stylesheet" href="styles/main.css">
+    <?php include_once("inc/analyticstracking.php") ?>
+    <script src="scripts/modernizr.js"></script>
     <script src="scripts/jquery.js"></script>
         <!-- build:js({app,.tmp}) scripts/main.js -->
         <script src="scripts/main.js"></script>
--- inc/application_top.php
+++ inc/application_top.php
@@ -4,18 +4,15 @@
     <meta charset="utf-8">
     <title>Kirsten Dodd</title>
     <meta name="viewport" content="width=device-width">
-
     <meta name="description" content="A Freelance Web Designer and Developer based in Victoria, BC.">
     <meta name="keywords" content="Website,Website Design,Website Development,Digital Design,Victoria Design">
     <meta name="author" content="Kirsten Dodd">
-
     <meta name="robots" content="noodp"/>
     <link rel="canonical" href="https://www.kirstendodd.com/"/>
     <meta property="og:locale" content="en_US"/>
     <meta property="og:type" content="website"/>
     <meta property="og:title" content="Kirsten Dodd | Web. Development. Design. | Victoria, B.C."/>
     <meta property="og:description" content="A Freelance Web Designer and Developer based in Victoria, BC. "/>
-
     <meta property="og:url" content="https://www.kirstendodd.com/"/>
     <meta property="og:site_name" content="Kirsten Dodd Freelance Web Designer and Developer"/>
     <meta property="og:image" content="images/favicon/favicon-96x96.png"/>
@@ -25,13 +22,7 @@
     <meta name="twitter:site" content="@kirstenjvd"/>
     <meta name="twitter:image" content="images/favicon/favicon-96x96.png"/>
     <meta name="twitter:creator" content="@kirstenjvd"/>
-
-
-    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,300,700' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Oswald:400,700,300' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700|Noto+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
     <link rel='shortcut icon' href='images/favicon/favicon.ico' type='image/x-icon'/ >
-
     <link rel="apple-touch-icon" sizes="57x57" href="images/favicon/apple-icon-57x57.png">
     <link rel="apple-touch-icon" sizes="60x60" href="images/favicon/apple-icon-60x60.png">
     <link rel="apple-touch-icon" sizes="72x72" href="images/favicon/apple-icon'/-72x72.png">
@@ -49,15 +40,6 @@
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
-    
-    <!-- build:css(.tmp) styles/main.css -->
-    <link rel="stylesheet" href="styles/normalize.css">
-    <link rel="stylesheet" href="styles/main.css">
-    <!-- endbuild -->
-    <!-- build:js scripts/vendor/modernizr.js -->
-    <script src="scripts/modernizr.js"></script>
-    <!-- endbuild -->
-
     <!--[if gte IE 9]>
       <style type="text/css">
         .gradient {
@@ -65,7 +47,6 @@
         }
       </style>
     <![endif]-->
-
     <!--[if lt IE 7]>
     <script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE7.js"></script>
     <![endif]-->
@@ -74,12 +55,9 @@
     <![endif]-->
     <!--[if lt IE 9]>
     <script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
-
     <![endif]-->
-
   </head>
   <body>
-  <?php include_once("inc/analyticstracking.php") ?>
     <!--[if lt IE 10]>
       <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->
